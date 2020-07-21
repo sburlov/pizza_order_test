f1828e8e6a03c1fef55840db5de9f83070be0ecc
Sergey Burlov <serqey@gmail.com>
2020-07-21 08:52:00 -0400

    Add timer

---
+++ target/generated-sources/groovy-stubs/main/org/camunda/bpm/latera/PizzaOrderApplication.java
@@ -0,0 +1,22 @@
+package org.camunda.bpm.latera;
+
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import groovy.lang.*;
+import groovy.util.*;
+
+@org.camunda.bpm.application.ProcessApplication(value="pizza_order") public class PizzaOrderApplication
+  extends org.camunda.bpm.application.impl.ServletProcessApplication  implements
+    groovy.lang.GroovyObject {
+;
+public  groovy.lang.MetaClass getMetaClass() { return (groovy.lang.MetaClass)null;}
+public  void setMetaClass(groovy.lang.MetaClass mc) { }
+public  java.lang.Object invokeMethod(java.lang.String method, java.lang.Object arguments) { return null;}
+public  java.lang.Object getProperty(java.lang.String property) { return null;}
+public  void setProperty(java.lang.String property, java.lang.Object value) { }
+public  org.camunda.bpm.engine.delegate.ExecutionListener getExecutionListener() { return (org.camunda.bpm.engine.delegate.ExecutionListener)null;}
+public  org.camunda.bpm.engine.delegate.TaskListener getTaskListener() { return (org.camunda.bpm.engine.delegate.TaskListener)null;}
+public  java.lang.String getLogLine(org.camunda.bpm.engine.delegate.DelegateExecution execution) { return (java.lang.String)null;}
+}
